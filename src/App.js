a5dbbf088c43eb44c95fae83841f8c6b86f0470d
Владимир Назаренко <rvpolska@gmail.com>
2023-01-22 13:05:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App (){
+
+const [names, setNames] = useState(data);
+const [showMore,setShowMore] = useState(false);
+
+const removeItem = (id) => {
+  let newItem = names.filter(name => name.id !==id);
+  setNames(newItem)
 }
 
+
+const showTextClick = (item) =>{
+  item.showMore = !item.showMore
+  setShowMore(!showMore)
+}
+
+return(
+  <div>
+  <div className="container">
+    <h1>My wife's {names.length} wishes!</h1>
+  </div>
+
+  
+  {names.map((item =>{
+    const {id, name,description, image, source, price,showMore} = item;
+
+    return(
+      <div key={id}>
+
+        <div className="container">
+        <h2>{id} - {name}</h2>
+        </div>
+
+        <div className="container">
+
+        <img src={image} width='400px' alt="img"/>
+
+        </div>
+      
+        <div className="container">
+       <p>{showMore ? description : description.substring(0, 50) + '...'}
+       <button onClick={() => showTextClick(item)}>{showMore ? 'Show less' : 'Show more'}</button>
+       </p>
+       </div>
+
+       <div className="container">
+       <p>source : {source}</p>
+       </div>
+
+       <div className="container">
+       <p className="price">price : {price} $</p>
+       </div>
+
+       <div className="container">
+      <button className="btn" onClick={() =>removeItem(id)}>Remove</button>
+
+      <button className="btn" onClick={() => setNames([])}>Remove all</button>
+
+       </div>
+       
+      </div>
+      
+    )
+  }))}
+  </div>
+)
+
+
+}
+
+
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/data.js
@@ -0,0 +1,63 @@
+export const data = [
+
+{
+ id: 1,
+//  smile: '😀',
+ name :'SHIRT WITH SIDE RUFFLES',
+ description : 'Crew neck Button ,fastening Buttoned cuffs',
+ image: 'https://static.massimodutti.net/3/photos//2022/I/0/1/p/5117/809/712/5117809712_2_5_16.jpg?t=1657721074493&impolicy=massimodutti-itxmediumhigh&imwidth=400&imformat=chrome',
+ source : 'massimodutti.com',
+ price : 200,
+ showMore : false
+},
+
+
+{
+    id: 2,
+    // smile: '🙂',
+    name :'DOUBLE-BREASTED SATIN SUIT BLAZER',
+    description : 'Satin finish Peak lapel collar, Double-breasted button fastening at the front',
+    image: 'https://static.massimodutti.net/3/photos//2022/I/0/1/p/6097/797/450/6097797450_2_5_16.jpg?t=1666796112293&impolicy=massimodutti-itxmediumhigh&imwidth=400&imformat=chrome',
+    source : 'massimodutti.com',
+    price: 150,
+    showMore : false
+   },
+   
+
+   {
+    id: 3,
+    // smile: '🙃',
+
+    name :'PRINTED DRESS WITH TEARDROP NECKLINE',
+    description : 'Made of 100% mulberry silk, Flowing fabric,Buttoned high collar, Button fastening,Buttoned cuffs, Buttoned cuffs with slits, Tie-up belt with buttons, Hem with back vent,Side vents at the hem, Made in Portugal',
+    image : 'https://static.massimodutti.net/3/photos//2023/V/0/1/p/6602/803/700/6602803700_2_9_16.jpg?t=1672832444418&impolicy=massimodutti-itxmediumhigh&imwidth=400&imformat=chrome',
+    source : 'massimodutti.com',
+    price: 170,
+    showMore : false
+   },
+
+   {
+    id: 4,
+    // smile: '😑',
+
+    name :'TROUSERS WITH TOPSTITCHING',
+    description :'Zip fly and button fastening, Two side pockets, Two patch pockets on the back,,Belt loops',
+    image: 'https://static.massimodutti.net/3/photos//2023/V/0/1/p/5017/567/710/5017567710_2_5_16.jpg?t=1667922570706&impolicy=massimodutti-itxmediumhigh&imwidth=400&imformat=chrome',
+    source : 'massimodutti.com',
+    price: 250,
+    showMore : false
+   },
+
+   {
+    id: 5,
+    // smile: '😢',
+
+    name :'CREW NECK OPEN-KNIT SWEATER',
+    description : 'Made of cotton and wool , Crew neck, Ribbed trims, Made in Italy',
+    image: 'https://static.massimodutti.net/3/photos//2023/V/0/1/p/5731/673/401/5731673401_2_5_16.jpg?t=1670847180468&impolicy=massimodutti-itxmediumhigh&imwidth=400&imformat=chrome',
+    source : 'massimodutti.com',
+    price: 100,
+    showMore : false
+   },
+   
+]
